b73f0b55dbeda1d865599a1042f7eed497ae5848
Aravind-kavati <aravindkavati83@gmail.com>
2023-09-23 00:43:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="CementStrength",
+    version="0.0.1",
+    author="aravind",
+    phno="812112",
+    author_email="aravindkavati83@gmail.com",
+    packages=find_packages()
+)
